d88837867442f7f552f5253c986eae5a08511f3a
Silas Schürger <100119945+CappedMonke@users.noreply.github.com>
2023-09-13 18:06:33 +0200

    update, added button

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    
+
+
+    public void CreateGrid()
+    {
+
+    }
+}
