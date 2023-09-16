d86026c339fe279272c09e8a803b264d64494faa
Silas Schürger <100119945+CappedMonke@users.noreply.github.com>
2023-09-16 19:46:49 +0200

    Added KeepObjectInView

---
+++ Assets/Scripts/KeepObjectInView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class KeepObjectInView : MonoBehaviour
+{
+    [SerializeField]
+    private float MaxDistanceBeforeMoving = 0.3f;
+    [SerializeField]
+    private float MovingTime = 0.1f;
+    
+    private Transform CameraTransform;
+    private float DistanceFromCamera;
+    private bool IsCoroutineRunning = false;
+
+    private void Start()
+    {
+        if (Camera.main == null)
+        {
+            return;
+        };
+        
+        CameraTransform = Camera.main.transform;
+        DistanceFromCamera = (transform.position - CameraTransform.position).magnitude;
+    }
+
+    private void Update()
+    {
+        var preObjectPosition = CameraTransform.position + CameraTransform.forward * DistanceFromCamera;
+        var distanceObjectPositionAndPreObjectPosition = (transform.position - preObjectPosition).magnitude;
+        
+        if (!IsCoroutineRunning && distanceObjectPositionAndPreObjectPosition > MaxDistanceBeforeMoving)
+        {
+            StartCoroutine(MoveToCameraCenter());
+        }
+    }
+
+    private IEnumerator MoveToCameraCenter()
+    {
+        IsCoroutineRunning = true;
+        
+        var elapsedTime = 0f;
+        var initialTransform = transform;
+
+        while (elapsedTime < MovingTime)
+        {
+            transform.position = Vector3.Lerp(initialTransform.position,
+                CameraTransform.position + CameraTransform.forward * DistanceFromCamera,
+                elapsedTime / MovingTime);
+            transform.rotation = Quaternion.Lerp(initialTransform.rotation,
+                CameraTransform.rotation,
+                elapsedTime / MovingTime);
+            
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = CameraTransform.position + CameraTransform.forward * DistanceFromCamera;
+        transform.rotation = CameraTransform.rotation;
+
+        IsCoroutineRunning = false;
+    }
+}
